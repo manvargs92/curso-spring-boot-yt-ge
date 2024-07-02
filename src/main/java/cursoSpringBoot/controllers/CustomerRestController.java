c1449d5ee7a7ed0c354c7d56134d3228965004bd
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-07-02 14:13:29 -0600

    Implementación de una API Rest.

---
+++ src/main/java/cursoSpringBoot/controllers/CustomerRestController.java
@@ -0,0 +1,26 @@
+package cursoSpringBoot.controllers;
+
+import cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerRestController {
+
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(123, "Gerardo López", "gerardol", "Mexico123"),
+            new Customer(456, "Alejandra García", "alegarcia", "contra123"),
+            new Customer(789, "Laura Sánchez", "lausanch", "secreto6789"),
+            new Customer(234, "Carlos Martínez", "carlitosm", "passw3456")
+    ));
+
+    @GetMapping("/clientes")
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+}
---
+++ src/main/java/cursoSpringBoot/domain/Customer.java
@@ -0,0 +1,51 @@
+package cursoSpringBoot.domain;
+
+// clase de tipo POJO
+public class Customer {
+
+    //atributos
+    private int id;
+    private String name;
+    private String userName;
+    private String password;
+
+    //constructor
+    public Customer(int id, String name, String userName, String password) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
